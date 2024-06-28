f9af9dfc14fa5a48a4ae426c09c836199e2bf0a6
Herr Oleh Bryl <olegbryl@gmail.com>
2024-06-28 10:14:29 +0200

    Almost

--- app/src/main/java/com/oleh/fakeshopapp/LoginActivity.java
+++ app/src/main/java/com/oleh/fakeshopapp/LoginActivity.java
@@ -37,7 +37,7 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+//
         usernameEditText = findViewById(R.id.usernameEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
 
